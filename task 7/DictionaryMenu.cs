defec78fd2a53ed8cdd0cb6d90b18e97db61caa7
solovyshn <solovyshn@gmail.com>
2021-10-19 01:20:13 +0300

    seven

---
+++ task 7/DictionaryMenu.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_7
+{
+    class DictionaryMenu
+    {
+        public Dictionary<string, List<double>> menu = new Dictionary<string, List<double>>();
+        public Dictionary<string, double> prices = new Dictionary<string, double>();
+        public DictionaryMenu(string path1, string path2)
+        {
+            ReadMenu(path1);
+            ReadPriceTag(path2);
+            menu = menu.OrderBy(item => item.Key).ToDictionary(item => item.Key, item => item.Value);
+        }
+
+        public void ReadMenu(string path)
+        {
+            if (!File.Exists(path)){
+                throw new FileNotFoundException();
+            }
+            using(StreamReader sr = File.OpenText(path))
+            {
+                for(int i=0; !sr.EndOfStream; i++)
+                {
+                    string str = sr.ReadLine();
+                    string[] parts = str.Split(' ');
+                    if (parts.Length == 2)
+                    {
+                        if (!menu.ContainsKey(parts[0]))
+                            menu.Add(parts[0], new List<double>() { Convert.ToDouble(parts[1]) });
+                        else
+                            menu[parts[0]].Add(Convert.ToDouble(parts[1]));
+                    }
+                }
+            }
+        }
+        public void ReadPriceTag(string path)
+        {
+            if (!File.Exists(path)){
+                throw new FileNotFoundException();
+            }
+            using (StreamReader sr = File.OpenText(path))
+            {
+                for(int i=0; !sr.EndOfStream; i++)
+                {
+                    string str = sr.ReadLine();
+                    string[] parts = str.Split(' ');
+                    prices.Add(parts[0], Convert.ToDouble(parts[1]));
+                }
+            }
+        }
+        public string PrintResult()
+        {
+            string res = "";
+            foreach(var item in menu)
+            {
+                res += item.Key+" ";
+                double sum = 0;
+                foreach(var val in item.Value)
+                {
+                    sum += val;
+                }
+                res += sum + " " + (sum * prices[item.Key]) + "\n";
+            }
+            return res;
+        }
+    }
+}
---
+++ task 7/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+            dictionary.Add("I", "Boy");
+            dictionary.Add("go", "run");
+            dictionary.Add("to", "to");
+            dictionary.Add("school", "cinema");
+
+            string text = "I go to school. I run to university. She go to school.";
+
+            string[] sentences = text.Split(new char[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
+            List<string> words = new List<string>();
+            for (int i = 0; i < sentences.Length; i++)
+            {
+                words.AddRange(sentences[i].Split());
+            }
+
+            foreach (var item in words)
+            {
+                Console.WriteLine(item);
+            }
+            for (int i = 0; i < words.Count; i++)
+            {
+                if (dictionary.ContainsKey(words[i]))
+                {
+                    words[i] = dictionary[words[i]];
+                }
+                else if (dictionary.ContainsValue(words[i]))
+                {
+                    words[i] = GetKeyFromValue(dictionary, words[i]);
+                }
+                else if (!String.IsNullOrEmpty(words[i]))
+                {
+                    Console.WriteLine("Dictionary doesn`t include \"{0}\". Do you want to add this word to dictionary?(1 - yes, 2 - no)", words[i]);
+                    int ch;
+                    ch = Convert.ToInt32(Console.ReadLine());
+                    if (ch == 1)
+                    {
+                        Console.WriteLine("Enter word, which can replace \"" + words[i] + "\"");
+                        string word;
+                        word = Console.ReadLine();
+                        dictionary.Add(words[i], word);
+                        words[i] = dictionary[words[i]];
+                    }
+
+                }
+            }
+            Console.WriteLine();
+            string result = "";
+            for (int i = 0; i < words.Count; i++)
+            {
+                if (string.IsNullOrEmpty(words[i]))
+                {
+                    result += ". ";
+                }
+                else
+                {
+                    result += words[i];
+                    if (i + 1 != words.Count)
+                    {
+                        if (!string.IsNullOrEmpty(words[i + 1]))
+                            result += ' ';
+                    }
+                    if (i + 1 == words.Count)
+                    {
+                        result += '.';
+                    }
+                }
+            }
+            Console.WriteLine("Result: " + result);
+            Console.WriteLine();
+
+            Console.WriteLine("Second task result:");
+            string pathMenu = @"C:\Alaska\studying\university\3 Sem\sigma\sigma_tasks\task 7\Menu.txt";
+            string pathPrice = @"C:\Alaska\studying\university\3 Sem\sigma\sigma_tasks\task 7\Price tag.txt";
+
+
+            DictionaryMenu dict = new DictionaryMenu(pathMenu, pathPrice);
+            Console.WriteLine(dict.PrintResult());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+        public static string GetKeyFromValue(Dictionary<string, string> dict, string value)
+        {
+            foreach (string keyVar in dict.Keys)
+            {
+                if (dict[keyVar] == value)
+                {
+                    return keyVar;
+                }
+            }
+            return null;
+        }
+    }
+}
